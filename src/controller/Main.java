b74981df34e290a401d47425c282e097e7cd3920
Alan Ensina <alanvinicius.ce@gmail.com>
2018-12-30 05:36:11 -0200

    Updating the main class to make the code simple and clean

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,40 +1,12 @@
 package controller;
 
-import java.util.Scanner;
-
-import model.Checker;
-import model.Player;
-import model.Reaper;
+import model.Initializer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		Reaper reaper = new Reaper();
-
-		reaper.phrase1();
-
-		String name = input.nextLine();
-
-		reaper.phrase2();
-
-		String city = input.nextLine();
-
-		Player player = new Player(name, city);
-		reaper.setPlayer(player);
-
-		reaper.phrase3();
-		reaper.phrase4();
-
-		Checker checker = new Checker(input.nextInt(), reaper);
-
-		checker.selectWord();
-		checker.arrayOrganizer();
-		reaper.phrase5();
-		
-		checker.startGame();		
-
-		input.close();
+	
+		Initializer.start();
 	}
 
 }
---
+++ src/model/Initializer.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.Scanner;
+
+public class Initializer {
+	
+	public Initializer() {}
+	
+	public static void start() {
+		Scanner input = new Scanner(System.in);
+		Reaper reaper = new Reaper();
+
+		reaper.phrase1();
+
+		String name = input.nextLine();
+
+		reaper.phrase2();
+
+		String city = input.nextLine();
+
+		Player player = new Player(name, city);
+		reaper.setPlayer(player);
+
+		reaper.phrase3();
+		reaper.phrase4();
+
+		Checker checker = new Checker(input.nextInt(), reaper);
+
+		checker.selectWord();
+		checker.arrayOrganizer();
+		reaper.phrase5();
+		
+		checker.startGame();		
+
+		input.close();
+	}
+
+}
